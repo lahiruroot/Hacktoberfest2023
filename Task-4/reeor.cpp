04cc78edcb43777969ec4f24cdefa64065b1ae15
Crazy Coder <65005446+crazy98@users.noreply.github.com>
2023-09-27 20:27:18 +0530

    Update reeor.cpp

--- Task-4/reeor.cpp
+++ Task-4/reeor.cpp
@@ -4,13 +4,25 @@ using namespace std;
 int main() {
     int n;
     cout << "Enter a number: ";
+    cin >> n;#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
     cin >> n;
 
-    int sum = 0;
-    for (int i = 1; i <= n; i++)
-        sum += i;
+    int sum = (n * (n + 1)) / 2;
 
-    cout << "Sum of numbers from 1 to " << n << " is: " << sum << endl
+    cout << "Sum of numbers from 1 to " << n << " is: " << sum << endl;
+
+    return 0;
+}
+
+
+    int sum = (n * (n + 1)) / 2;
+
+    cout << "Sum of numbers from 1 to " << n << " is: " << sum << endl;
 
     return 0;
 }
